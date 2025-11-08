68653ff05b6badfd38e248481fc7826d34849e32
changmin <ml505050@naver.com>
2025-11-08 14:46:19 +0900

    [FEAT] 로그인 컨트롤러

---
+++ src/main/java/mju/library/domain/member/MemberController.java
@@ -0,0 +1,13 @@
+package mju.library.domain.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login"; // templates/login.html
+    }
+}
