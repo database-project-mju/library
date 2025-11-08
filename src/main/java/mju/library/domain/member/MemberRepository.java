c20d7c5a2b15f2928d267b35cfa5a17b8f37b97e
changmin <ml505050@naver.com>
2025-11-08 14:44:40 +0900

    [FEAT] 관리자 페이지 기본 구현

--- src/main/java/mju/library/domain/member/MemberRepository.java
+++ src/main/java/mju/library/domain/member/MemberRepository.java
@@ -3,6 +3,9 @@ package mju.library.domain.member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByStudentNo(String studentNo);
 }
